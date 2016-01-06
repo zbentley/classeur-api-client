4113795be2334098db71216e109e6478ff649ac1
Zac Bentley <zbbentley@gmail.com>
2016-01-06 17:31:32 -0500

    Make tests use mocha

--- test/api-client-test.js
+++ test/api-client-test.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const _ = require('lodash'),
-	ApiClient = require('../lib/classeur-api-client'),
-	eyes = require('eyes'),
-	fs = require('fs-extra'),
-	p = _.bind(eyes.inspect, eyes),
-	pathJoin = require('path').join,
-	should = require('should'),
-	credentials = fs.readJsonSync(pathJoin(__dirname, 'credentials.json'));
+    ApiClient = require('../lib/classeur-api-client'),
+    eyes = require('eyes'),
+    fs = require('fs-extra'),
+    p = _.bind(eyes.inspect, eyes),
+    should = require('should'),
+    credentials = fs.readJsonSync(require('path').join(__dirname, 'credentials.json'));
 
-
-p(credentials);
+describe('constructor', function() {
+    it('defaults host correctly', function () {
+        let conn = new ApiClient(credentials);
+        conn.root.should.equal("https://app.classeur.io/api/v1/");
+    });
+});
